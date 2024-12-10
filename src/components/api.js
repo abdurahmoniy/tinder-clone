a1758ef2618b1606887863af0867e50e9bc49233
abdurahmoniy <azizbekabdurahmoniy@gmail.com>
2024-12-10 09:58:43 +0500

    api update

--- src/components/api.js
+++ src/components/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: 'https://533f-213-230-80-201.ngrok-free.app/api',
+  baseURL: 'https://9409-213-230-80-129.ngrok-free.app/api',
   withCredentials: true,
   headers: {
     'ngrok-skip-browser-warning': 'true',
